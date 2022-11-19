614203d2b171620f7cfbdeeb2abe78790fc7c817
FuexFollets <fuex9follets@gmail.com>
2022-11-18 22:08:42 -0500

    implemented money division

--- Units/money.hpp
+++ Units/money.hpp
@@ -17,13 +17,12 @@ namespace unit {
         std::string_view currency_name;
         const char* locale_name {"en_US.UTF-8"};
 
-        operator float ();
-        operator double ();
+        template <std::floating_point T> operator T () requires std::convertable_to<cents, T>;
+        template <std::integral T> operator T () requires std::convertable_to<cents, T>;
+
         operator const char* ();
         operator std::string ();
         operator std::string_view ();
-
-        template <std::integral T = std::size_t> T cents();
 
         friend std::ostream& operator<<(std::ostream&, const money&); friend bool operator==(const money&, const money&);
         template <typename T> friend money operator+(const money&, const T&);
@@ -41,9 +40,16 @@ namespace unit {
         cents whole_subdivision; // ex: cents
     };
 
+    template <std::floating_point T>
+        money::operator T () requires std::convertable_to<cents, T> {
+        return static_cast<T>(whole_subdivision) / 100;
+    }
 
-    money::operator float () { return static_cast<float>(whole_subdivision) / 100; }
-    money::operator double () { return static_cast<double>(whole_subdivision) / 100; }
+    template <std::integral T>
+        money::operator T () requires std::convertable_to<cents, T> {
+        return static_cast<T>(whole_subdivision);
+    }
+
     money::operator std::string () { return (std::stringstream() << *this).str(); }
     money::operator const char* () { return static_cast<std::string>(*this).c_str(); }
     money::operator std::string_view () { return std::string_view(static_cast<const char*>(*this)); }
