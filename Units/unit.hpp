26c4dbe3f319e46bf27b9daf189cb9c70d18330c
FuexFollets <fuex9follets@gmail.com>
2022-11-19 10:52:23 -0500

    add: inheritable unit with metric prefix

--- Units/unit.hpp
+++ Units/unit.hpp
@@ -1,16 +1,47 @@
 #pragma once
 
 #include <cstdint>
+#include <ratio>
+#include <concepts>
 
 namespace unit { 
+
+
+    template <std::integral T>
+        bool power_of_10(T whole_number) {
+        if (whole_number == 1) return true;
+
+        while (whole_number != 0) {
+            if (whole_number == 1) return true;
+            if (whole_number % 10 != ) return false;
+
+            whole_number /= 10;
+        }
+
+        return false;
+    }
+
+
+    struct no_prefix {};
+
+
+    template <typename T>
+        concept metric_prefix_ratio = requires {
+            T::num;
+            T::den;
+        } && power_of_10(T::num) && power_of_10(T::den) ||
+        std::same_as<T, no_prefix>;
+
+
     enum QuantitativeType { 
         Whole = std::size_t,
-        Integer = int,
-        Real = long double
+        Integer = long long,
+        Real = long double,
     };
 
-    template <QuantitativeType T>
+    template <QuantitativeType T, metric_prefix_ratio R = no_prefix>
     struct unit {
+        using metrix_prefix = R;
         T value {};
     };
 }
