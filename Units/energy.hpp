cea49f73db522af56a19427ba6c3450820b36b56
Fuexfollets <fuex9follets@gmail.com>
2022-11-22 09:21:15 -0500

    added template default argument and joule instantiatipn

--- Units/energy.hpp
+++ Units/energy.hpp
@@ -3,6 +3,8 @@
 #include "unit.hpp"
 
 namespace unit { namespace energy {
-    template <metric_prefix_ratio Prefix>
+    template <metric_prefix_ratio Prefix = no_prefix>
         struct basic_joule : public basic_unit<basic_joule<Prefix>, QuantitativeType::Continuous, Prefix> {};
+
+    using joule = basic_joule;
 }}
