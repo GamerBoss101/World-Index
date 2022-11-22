2455872c7842a77b6d4e61dbbc3c4ef94146dfbc
Fuexfollets <fuex9follets@gmail.com>
2022-11-22 09:24:52 -0500

    added joule instantiation

--- Units/energy.hpp
+++ Units/energy.hpp
@@ -1,10 +1,14 @@
 #pragma once
 
+#include <ratio>
+
 #include "unit.hpp"
 
 namespace unit { namespace energy {
     template <metric_prefix_ratio Prefix = no_prefix>
         struct basic_joule : public basic_unit<basic_joule<Prefix>, QuantitativeType::Continuous, Prefix> {};
 
-    using joule = basic_joule;
+    using joule = basic_joule<no_prefix>;
+    
+    using djoule = std::basic_joule<std::deca>;
 }}
